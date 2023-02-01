cb3df281158869ae796f5f39e6e4cbb51c44b6ca
OBenhamIW <102970132+OBenhamIW@users.noreply.github.com>
2023-02-01 15:40:28 +0000

    Tidying test cases

--- tests/extractor_tests/test_extractor.py
+++ tests/extractor_tests/test_extractor.py
@@ -1,66 +1,68 @@
 import os
 import numpy as np
+import unittest
 
 from extractor.extract_frames import extract_frames_from_video
 from extractor.extract_faces import extract_faces_from_frames
-from extractor.utils import pil_loader, save_landmarks_on_image, mkdir_or_delete_existing_files
+from extractor.utils import (
+    pil_loader,
+    save_landmarks_on_image,
+    mkdir_or_delete_existing_files,
+)
 
 
-def test_extract_frames():
-    # clear directory
-    input_folder = "workspace_test/extract_frames_test/"
-    for image in os.listdir(input_folder):
-        if image.endswith(".jpg"):
-            os.remove(input_folder + image)
+class TestExtractFrames(unittest.TestCase):
+    def setUp(self) -> None:
+        self.input_folder = "workspace_test/extract_frames_test/"
+        for image in os.listdir(self.input_folder):
+            if image.endswith(".jpg"):
+                os.remove(self.input_folder + image)
 
-    # assembled
-    expected_number_of_frames = 585
+    def test_extract_frames_from_video(self) -> None:
+        expected_number_of_frames = 585
+        extract_frames_from_video(
+            input_file=self.input_folder + "JMT.mp4",
+            output_dir=self.input_folder,
+            output_ext="jpg",
+        )
+        input_image_paths = [
+            os.path.join(self.input_folder, x)
+            for x in os.listdir(self.input_folder)
+            if x.endswith(".jpg")
+        ]
+        assert len(input_image_paths) == expected_number_of_frames
 
-    # act
-    extract_frames_from_video(
-        input_file=input_folder + "JMT.mp4",
-        output_dir=input_folder,
-        output_ext="jpg"
-    )
-    input_image_paths = [os.path.join(input_folder, x) for x in os.listdir(input_folder) if x.endswith(".jpg")]
 
-    # assert
-    assert len(input_image_paths) == expected_number_of_frames
+class TestExtractFace(unittest.TestCase):
+    def setUp(self) -> None:
+        self.input_folder = "workspace_test/extract_faces_test/"
+        self.output_folder = "workspace_test/extract_faces_validation/"
+        self.images_folder = self.input_folder + "aligned/"
+        self.landmarks_folder = self.input_folder + "landmarks/"
 
+    def test_extract_faces_from_frames(self) -> None:
+        number_of_landmarks_files = number_of_faces = 14
 
-def test_extract_faces():
-    input_folder = "workspace_test/extract_faces_test/"
-    output_folder = "workspace_test/extract_faces_validation/"
-    images_folder = input_folder + "aligned/"
-    landmarks_folder = input_folder + "landmarks/"
-
-    # assemble
-    number_of_landmarks_files = number_of_faces = 14
-
-    # act
-    extract_faces_from_frames(
-        input_path=input_folder,
-        images_output_path=images_folder,
-        landmarks_output_path=landmarks_folder,
-        max_faces_from_image=1,
-        image_size=512,
-        jpeg_quality=100
-    )
-
-    # visual validation
-    mkdir_or_delete_existing_files(output_folder)
-    assert len(os.listdir(output_folder)) == 0
-    for image in os.listdir(images_folder):
-        img = pil_loader(images_folder + image)
-        landmarks = np.load(landmarks_folder + image.rstrip(".jpg") + ".npy")
-        save_landmarks_on_image(
-            image=img,
-            landmarks=landmarks,
-            file_path=output_folder+ image
+        extract_faces_from_frames(
+            input_path=self.input_folder,
+            images_output_path=self.images_folder,
+            landmarks_output_path=self.landmarks_folder,
+            max_faces_from_image=1,
+            image_size=512,
+            jpeg_quality=100,
         )
 
-    # assert
-    assert len(os.listdir(output_folder)) == number_of_faces
-    assert len(os.listdir(images_folder)) == number_of_faces
-    assert len(os.listdir(landmarks_folder)) == number_of_landmarks_files
+        # visual validation
+        mkdir_or_delete_existing_files(self.output_folder)
+        assert len(os.listdir(self.output_folder)) == 0
+        for image in os.listdir(self.images_folder):
+            img = pil_loader(self.images_folder + image)
+            landmarks = np.load(self.landmarks_folder + image.replace(".jpg", ".npy"))
+            save_landmarks_on_image(
+                image=img, landmarks=landmarks, file_path=self.output_folder + image
+            )
 
+        # assert
+        assert len(os.listdir(self.output_folder)) == number_of_faces
+        assert len(os.listdir(self.images_folder)) == number_of_faces
+        assert len(os.listdir(self.landmarks_folder)) == number_of_landmarks_files
--- tests/trainer_tests/test_loader.py
+++ tests/trainer_tests/test_loader.py
@@ -10,7 +10,7 @@ class TestImageDS(unittest.TestCase):
         self.custom_ds = CustomImageDataset()
         self.random_int = random.choice(range(len(self.custom_ds)))
 
-    def test_get_item(self):
+    def test_get_item(self) -> None:
         sample = self.custom_ds[self.random_int]
         assert len(sample) == 6
         expected_image_shape = (
@@ -26,7 +26,7 @@ class TestImageDS(unittest.TestCase):
         assert sample[Params.target_dst].shape == expected_image_shape
         assert sample[Params.target_dst_mask].shape == expected_mask_shape
 
-    def test_create_dst_sublists(self):
+    def test_create_dst_sublists(self) -> None:
         test_dst_dir = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
         test_src_dir = ["x", "y", "z"]
         sublists = self.custom_ds.create_dst_sublist(
@@ -39,9 +39,8 @@ class TestCustomDL(unittest.TestCase):
     def setUp(self) -> None:
         self.custom_dl = CustomDataLoader()
 
-    def test_run(self):
-        for sample in self.custom_dl.run():
-            break
+    def test_run(self) -> None:
+        sample = next(iter(self.custom_dl.run()))
         expected_image_shape = (
             Params.batch_size,
             Params.image_input_channels,
